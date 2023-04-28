cc9c472d1e8884a4777b35e385ebc706d86a76e3
youngmin yoo <yym3055@gmail.com>
2023-04-28 15:52:17 +0900

    adfas

--- src/app/routes/users.router.ts
+++ src/app/routes/users.router.ts
@@ -3,4 +3,22 @@ import { UserController } from '../controllers';
 
 const router: Router = express.Router();
 
+/* Get all users */
+router.get('/', UserController.getAllUsers);
+
+/* Get user by ID */
+router.get('/:id', UserController.getUserById);
+
+/* Update user by ID */
+router.put('/:id', UserController.updateUserById);
+
+/* Delete user by ID */
+router.delete('/:id', UserController.deleteUserById);
+
+/* Create new user */
+router.post('/signup', UserController.createUser);
+
+/* Login user */
+router.post('/login', UserController.loginUser);
+
 export const usersRouter: Router = router;
