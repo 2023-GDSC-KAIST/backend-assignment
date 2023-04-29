c4ccc36407ffaac26370b48dd77de6972093f675
윤지훈 <129292999+younjihoon@users.noreply.github.com>
2023-04-29 22:43:15 +0900

    Add files via upload

--- src/app/routes/users.router.ts
+++ src/app/routes/users.router.ts
@@ -4,3 +4,24 @@ import { UserController } from '../controllers';
 const router: Router = express.Router();
 
 export const usersRouter: Router = router;
+
+router.get('/', UserController.get);
+
+router.get('/:id', UserController.list);
+
+router.put('/:id',UserController.modifyByID);
+
+router.delete('/:id',UserController.delUser);
+
+router.post('/signup', UserController.create);
+
+router.post('/login', UserController.login);
+/*
+- GET: /users // DB에 있는 모든 유저들의 이름을 반환합니다.
+- GET: /users/:id // DB에서 해당 id (mongoDB에서 생성되는 _id)를 가진 유저를 반환합니다.
+- PUT: /users/:id // DB에서 해당 id를 가진 유저의 회원정보를 수정합니다.
+- DELETE: /users/:id // DB에서 해당 id를 가진 유저의 회원탈퇴를 수행합니다.
+
+- POST: /users/signup // username, password, name, email field를 포함하는 document를 생성합니다.
+- POST: /users/login // username, password를 받아 로그인을 수행합니다.
+*/
--- src/app/schemas/user.schema.ts
+++ src/app/schemas/user.schema.ts
@@ -1,13 +1,46 @@
 import mongoose, { Model, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 export interface IUser {
-
+  username: string;
+  userID: string;
+  password: string;
+  email: string;
 }
 
 interface UserModel extends Model<IUser> {}
 
-const UserSchema = new Schema<IUser, UserModel>({
-
+const UserSchema = new Schema<IUser, UserModel>(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    userID: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+UserSchema.pre<IUser>('save', async function (next) {
+  const user = this;
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
 });
+
+
 
 export const User = mongoose.model<IUser, UserModel>('User', UserSchema);
