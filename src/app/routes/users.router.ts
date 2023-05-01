dc50b233818ba1841975d018d9d7acfd50d6bc67
Byunk <clearman001@gmail.com>
2023-05-01 22:54:54 +0900

    Build RESTful API and encrypt password

--- src/app/routes/users.router.ts
+++ src/app/routes/users.router.ts
@@ -3,4 +3,15 @@ import { UserController } from '../controllers';
 
 const router: Router = express.Router();
 
+router.get('/', UserController.get);
+
+router
+  .route('/:id')
+  .get(UserController.getByUserId)
+  .put(UserController.updateByUserId)
+  .delete(UserController.deleteByUserId);
+
+router.post('/signup', UserController.signup);
+router.post('/login', UserController.login);
+
 export const usersRouter: Router = router;
